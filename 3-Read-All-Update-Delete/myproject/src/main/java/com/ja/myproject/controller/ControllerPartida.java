5201f0ec4f34776492084e10b3c4718b6b58e142
Jardel Andrade <7.jardel.andrade@gmail.com>
2024-12-05 20:25:24 -0300

    Read All - Update - Delete

---
+++ 3-Read-All-Update-Delete/myproject/src/main/java/com/ja/myproject/controller/ControllerPartida.java
@@ -0,0 +1,92 @@
+package com.ja.myproject.controller;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ja.myproject.model.ModelPartida;
+import com.ja.myproject.model.RepositoryPartida;
+
+import jakarta.transaction.Transactional;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/partida")
+public class ControllerPartida {
+
+    @Autowired
+    private RepositoryPartida repositoryPartida;
+
+    @GetMapping("/")
+    public ModelAndView readAll (ModelAndView modelAndView) {
+        List<ModelPartida> partidas = repositoryPartida.findAll();
+        String viewName = "partida/readAll";
+        String partidasName = "partidas";
+        modelAndView.setViewName(viewName);
+        modelAndView.addObject(partidasName, partidas);
+        return modelAndView;
+    }
+
+    @GetMapping("/insertDados")
+    public ModelAndView insertDados(Long id, ModelAndView modelAndView){
+        System.out.println();
+        System.out.println("insertDados");
+        System.out.println();
+        String viewName = "partida/create";
+        if(id!= null){
+            ModelPartida modelPartida = repositoryPartida.getReferenceById(id);
+            String modelPartidaName = "modelPartida";
+            modelAndView.addObject(modelPartidaName, modelPartida);
+        }
+        modelAndView.setViewName(viewName);
+        return modelAndView;
+    }
+
+    @PostMapping("/save")
+    public String saveInsertDados (ModelPartida modelPartida) {
+        String viewName = "redirect:/partida/";
+        repositoryPartida.save(modelPartida);
+        return viewName;
+    }
+
+    @GetMapping("/readOne")
+    public ModelAndView readOne (Long id) {
+        ModelAndView modelAndView = new ModelAndView();
+        ModelPartida modelPartida = repositoryPartida.getReferenceById(id);
+        String viewName = "partida/readOne";
+        String partidaName = "modelPartida";
+        modelAndView.addObject(partidaName, modelPartida);
+        modelAndView.setViewName(viewName);
+        return modelAndView;
+    }
+
+    @PutMapping("/save")
+    @Transactional
+    public String saveUpdateInsertDados (Long id, String localRealizacao, LocalDate dataRealizacao, LocalTime timeBegin, LocalTime timeEnd){
+        System.out.println();
+        System.out.println("put cadastrar transactional atualizar");
+        System.out.println();
+        ModelPartida modelPartida = repositoryPartida.getReferenceById(id);
+        modelPartida.setLocalRealizacao(localRealizacao);
+        modelPartida.setDataRealizacao(dataRealizacao);
+        modelPartida.setTimeBegin(timeBegin);
+        modelPartida.setTimeEnd(timeEnd);
+        return "redirect:/partida/";
+    }
+
+    @DeleteMapping("/delete")
+    public String delete (Long id){
+        String viewName = "redirect:/partida/";
+        repositoryPartida.deleteById(id);
+        return viewName;
+    }
+}
--- 3-Read-All-Update-Delete/myproject/src/main/java/com/ja/myproject/model/ModelPartida.java
+++ 3-Read-All-Update-Delete/myproject/src/main/java/com/ja/myproject/model/ModelPartida.java
@@ -28,6 +28,14 @@ public class ModelPartida {
         this.timeEnd = timeEnd;
     }
 
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        this.Id = id;
+    }
+
     public String getLocalRealizacao() {
         return localRealizacao;
     }
